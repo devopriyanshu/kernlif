f53e5e3484fcd29fd142ac5264a609af37f34bbe
devopriyanshu <priyanshupatel1902@gmail.com>
2025-12-12 13:27:58 +0530

    url

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -57,7 +57,7 @@ const LoginSignup = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:4000/auth/google";
+    window.location.href = "http://www.mywellnest.app/auth/google";
   };
 
   return (
